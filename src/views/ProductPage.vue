d4d2ca89c1c85ee666b14027af6c4fa4199cd5a7
mahmoudAdham96 <88621466+mahmoudAdham96@users.noreply.github.com>
2023-02-21 15:44:02 +0200

    handel search

--- src/views/ProductPage.vue
+++ src/views/ProductPage.vue
@@ -94,7 +94,7 @@ export default {
     return {
       products: [],
       pages: [],
-      search: "t-shert",
+      search: "",
       isLoaded: false,
       currentPage: 1,
       perPage: 48,
